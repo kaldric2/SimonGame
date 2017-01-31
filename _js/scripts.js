4acddbd514ba64ba06630aa822ee92fcbd8198a4
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-01-30 21:08:46 -0800

    Initial commit

---
+++ _js/scripts.js
@@ -0,0 +1,6 @@
+/*
+    https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
+    https://s3.amazonaws.com/freecodecamp/simonSound2.mp3
+    https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
+    https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
+*/
