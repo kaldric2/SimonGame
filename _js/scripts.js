dd951f9c942a5108ce11106a51069d3b32f7a874
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-02-01 23:26:41 -0800

    js function buildout

--- _js/scripts.js
+++ _js/scripts.js
@@ -7,18 +7,85 @@
 
 
 // Create an array to store user button presses
-var userArray = [];
 var gameArray = [];
+var btnArray = [];
+var btnSounds = [
+                    "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+                    "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+                    "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+                    "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+                ];
+var seqNum = -1;
+var roundNum = 1;
+var strictMode = 0;
 
-// Create an array of twenty random numbers between 0 and 3
+// Computer functions
 function createGameArray() {
     for (var i = 0; i < 20; i++) {
         gameArray.push(Math.floor(Math.random() * 4));
     }
 }
 
-createGameArray();
-console.log(gameArray);
+function calculateTimeDelay() {
+    var timeDelay = 0;
+    if (roundNum < 5) { timeDelay = 1000; }
+    else if (roundNum < 9) { timeDelay = 750; }
+    else if (roundNum < 13) { timeDelay = 500; }
+    else { timeDelay = 250; }
+    return timeDelay;
+}
+
+function cpuShowSequence() {
+    for (let i = 0; i < roundNum; i++) {
+        setTimeout(lightButton(true, i), calculateTimeDelay());
+    }
+}
+
+// Shared functions
+function lightButton(isCorrect, i) {
+    // TODO: light up the button at btnArray[i]
+    btnArray[i].classList.add("btnLit");
+    playSound(isCorrect, i);
+    btnArray[i].classList.remove("btnLit");
+}
+
+function playSound(isCorrect, idx) {
+    if (!isCorrect) {
+        // TODO: play lose sound
+    } else {
+        // TODO: play btnSounds[idx]
+    }
+}
+
+
+// User functions
+function isCorrect(s, i) {
+    if (i != gameArray[s]) {
+        return false;
+    }
+    return true;
+}
+
+function wrongAnswer() {
+    seqNum = -1;
+    if (strictMode) {
+        roundNum = 1;
+    }
+    cpuShowSequence();
+}
+
+function onBtnPress(btn) {
+    seqNum++;
+    var idx = btnArray.indexOf(btn);
+    var isCorrect = isCorrect(seqNum, idx);
+    lightButton(isCorrect, idx);
+
+    if (seqNum == roundNum-1) {
+        if (isCorrect & roundNum < 20) { roundNum++; }
+        seqNum = -1;
+        cpuShowSequence();
+    }
+}
 
 // Create a function to compare the user array to the game array upon each subsequent user button press
 // Trigger button color animation on press or computer move
@@ -27,5 +94,8 @@ console.log(gameArray);
 // Handle errors based on strict/non-strict mode
 
 // Handle start conditions
+    // add buttons to btnArray
+    // add click listeners to buttons that trigger color animation(?) and sound
+
 // Handle reset conditions
 // Display current score (ie. length of last successful sequence in current game)
