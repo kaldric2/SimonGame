5e1c65984757e4e4b6ff8284ad04a0cb7beddbd7
Kevin Aldrich <kevin.aldrich@gmail.com>
2017-02-01 22:17:00 -0800

    added generator for 20-member array of random integers between 0 and 3, inclusive

--- _js/scripts.js
+++ _js/scripts.js
@@ -4,3 +4,28 @@
     https://s3.amazonaws.com/freecodecamp/simonSound3.mp3
     https://s3.amazonaws.com/freecodecamp/simonSound4.mp3
 */
+
+
+// Create an array to store user button presses
+var userArray = [];
+var gameArray = [];
+
+// Create an array of twenty random numbers between 0 and 3
+function createGameArray() {
+    for (var i = 0; i < 20; i++) {
+        gameArray.push(Math.floor(Math.random() * 4));
+    }
+}
+
+createGameArray();
+console.log(gameArray);
+
+// Create a function to compare the user array to the game array upon each subsequent user button press
+// Trigger button color animation on press or computer move
+// Trigger button sound on press or computer move
+
+// Handle errors based on strict/non-strict mode
+
+// Handle start conditions
+// Handle reset conditions
+// Display current score (ie. length of last successful sequence in current game)
